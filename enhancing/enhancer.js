31bf22c7ad5e81be07c1a673949ff8a15fec4a52
Alison Law <alisparklaw@gmail.com>
2020-03-25 14:12:04 +0000

    repair and success enhancers and tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,66 @@ module.exports = {
   get,
 };
 
+
+
 function succeed(item) {
-  return { ...item };
+  let enhancedItem = item.enhancement +1
+  if (item.enhancement >= 20){
+    return { ...item}
+  }
+  else{
+    return {...item, enhancement:enhancedItem}
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  
+  let unEnhancedItem = item.enhancement -1
+let durabilityDec5 = item.durability-5
+let durabilityDec10 = item.durability-10
+
+  if(item.enhancement < 15){
+return { ...item,
+  durability:durabilityDec5
+}
+  }else{
+    if(item.enhancement <= 16){
+      return { ...item,
+        durability:durabilityDec10,
+       
+      }
+    }else{
+      return { ...item,
+        durability:durabilityDec10,
+        enhancement:unEnhancedItem
+      }
+
+    }
+
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability:100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+console.log(fail({
+  name:'a name',
+  durability:20,
+  enhancement:10
+}))
+
+console.log(fail({
+  name:'a name',
+  durability:20,
+  enhancement:15
+}))
+console.log(fail({
+  name:'a name',
+  durability:20,
+  enhancement:17
+}))
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,83 @@
+/* eslint-disable no-undef */
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancers', () => {
+    
+    describe('repair', () => {
+      it('runs without crashing', () => {
+        enhancer.repair({
+            name:'a name',
+            durability:20,
+            enhancement:50
+          
+          })
+      })
+      it('returns object with durability:100', () => {
+        const expectedOutput = {
+            name:'a name',
+            durability:100,
+            enhancement:50
+          }
+        expect(enhancer.repair({
+            name:'a name',
+            durability:20,
+            enhancement:50
+          
+          }))
+          .toEqual(expectedOutput)
+      })
+    })
+
+    describe('succeed', () => {
+        it('runs without crashing', () => {
+            enhancer.succeed({
+                name:'a name',
+                durability:20,
+                enhancement:10 
+              })
+          })
+          it('runs without crashing', () => {
+            enhancer.succeed({
+                name:'a name',
+                durability:20,
+                enhancement:21 
+              })
+          })
+     
+          it('increments enhancement by 1 conditionally', () => {
+            const expectedOutput2 = {
+                name:'a name',
+                durability:20,
+                enhancement:11
+              }
+              const expectedOutput3 = {
+                name:'a name',
+                durability:20,
+                enhancement:20
+              }
+              expect(enhancer.succeed({
+                name:'a name',
+                durability:20,
+                enhancement:10
+              }))
+              .toEqual(expectedOutput2)
+
+              expect(enhancer.succeed({
+                name:'a name',
+                durability:20,
+                enhancement:20
+              }))
+              .toEqual(expectedOutput3)
+
+          })
+
+
+    })
+
+    // describe('fail', () => {
+
+
+    // })
+
+
+})
